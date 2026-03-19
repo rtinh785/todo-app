9304697db305ae68a8aa7b5d6e53b7145308fbfd
rtinh785 <rtinh785@gmail.com>
2026-03-20 03:01:29 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import devtoolsJson from 'vite-plugin-devtools-json'
+
+export default defineConfig({
+  css: {
+    devSourcemap: true
+  },
+  server: {
+    port: 3000
+  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()]
+})
